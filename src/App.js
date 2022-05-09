d76cd0e7cce7660d6db6845006f1f7076e4899b0
Phakorn Praditthamrong <iketu65@gmail.com>
2022-05-10 00:47:00 +0700

    commit 1

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import './App.css';
+import React, { useState } from 'react';
+import keys from './keys'
+
+const api = {
+  key: keys.API_KEY,
+  base: keys.BASE_URL
+}
+
+function App() {
+
+  const dataBuild = (d) => {
+    let date = String(new window.Date());
+    date = date.slice(4, 15)
+    return date;
+  }
+
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
+        .then((res) => res.json())
+        .then((results) => {
+          setQuery("");
+          setWeather(results)
+          console.log(results);
+        });
+    }
+  }
+
+  return (
+    <div className={
+      typeof weather.main != "undefined"
+        ? weather.main.temp > 18
+          ? "App hot"
+          : "App cold"
+        : "App"
+    }>
+      <main>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-bar"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className='location-container'>
+              <div className='location'>
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className='date'>{dataBuild()}</div>
+            </div>
+            <div className='weather-container'>
+              <div className='temperature'>
+                {Math.round(weather.main.temp)}°C
+              </div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    API_KEY: "6f05fc5dfb72334cfcbace34d7e4a278",
+    BASE_URL: "https://api.openweathermap.org/data/2.5/"
+}
